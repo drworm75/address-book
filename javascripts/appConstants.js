c460cf55f4bc8b2992dd61c4e4b71e35cbfd29ea
drworm75 <dwgames@gmail.com>
2017-05-15 17:04:45 -0500

    Firebase setup.  Calling fb and printing contacts to dom.  New Contact page set up.

---
+++ javascripts/appConstants.js
@@ -0,0 +1,8 @@
+app.constant("FIREBASE_CONFIG", {
+    apiKey: "AIzaSyAtGuJjMOMgIl5H2xXNByX2_n18IuWy1VE",
+    authDomain: "address-book-9adba.firebaseapp.com",
+    databaseURL: "https://address-book-9adba.firebaseio.com",
+    projectId: "address-book-9adba",
+    storageBucket: "address-book-9adba.appspot.com",
+    messagingSenderId: "167162543675"
+});
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,28 +1,64 @@
-var app = angular.module("AddressBook", []);
+app.run((FIREBASE_CONFIG) => {
+  firebase.initializeApp(FIREBASE_CONFIG);
+});
 
-app.controller ("ContactInfo", ($scope) => {
+app.controller("NavCtrl", ($scope) => {
+	$scope.navItems = [{name: "Logout"}, {name: "Show All Contacts"}, {name: "Search Contacts"}, {name: "New Contact"}];
+});
 
-	$scope.contacts = [
-		{
-			name: "Terry Moore",
-			phone: "615-555-7771",
-			job: "Artist",
-		},
-		{
-			name: "Jonathan Coulton",
-			phone: "615-555-9292",
-			job: "Singer",
-		},
-		{
-			name: "Steve Womack",
-			phone: "615-555-5872",
-			job: "Writer",
-		},
-		{
-			name: "Stephen Lackey",
-			phone: "615-555-4444",
-			job: "Director",
-		},
-	];
+app.controller ("ContactCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+	$scope.showContactView = true;
+	$scope.contacts = [];
+	$scope.newContact = (name) => {
+		console.log("name =", name);
+		if (name === "New Contact") {
+			$scope.showContactView =  false;
+		}
+		if (name === "Show All Contacts") {
+			$scope.showContactView =  true;
+
+		}
+	};
+
+
+
+	let getContactList = () => {
+		let info = [];
+		return $q((resolve, reject) => {
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/contacts.json`)
+			.then((fbItems) => {
+				var contactCollection = fbItems.data;
+				Object.keys(contactCollection).forEach((key) => {
+					contactCollection[key].id=key;
+					info.push(contactCollection[key]);
+				});
+				resolve(info);
+			})
+			.catch((error) => {
+				reject(error);
+			});
+		});
+	};
+
+	let getContacts = () => {
+		getContactList().then((info) => {
+			$scope.contacts = info;
+			console.log("scope contacts", $scope.contacts);
+		}).catch((error) => {
+			console.log("get error", error);
+		});
+	};
+
+	getContacts();
+
+	$scope.addNewContact = () => {
+	    postNewContact($scope.newContact).then(() => {
+			$scope.newContact = {};
+			$scope.showContactView =true;
+			getContacts();
+	    }).catch((error) => {
+			console.log("Add error", error);
+	    });
+	  };
 });
 
